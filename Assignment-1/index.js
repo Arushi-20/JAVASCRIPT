6537eaf0b366c080b44ec951c03e80875d5daa98
Arushi Sharma <avpsharma3012@gmail.com>
2026-06-23 08:56:09 +0530

    Assignment-1

---
+++ Assignment-1/index.js
@@ -0,0 +1,44 @@
+let a=2
+let b=5
+
+//1
+console.log(a+b);
+
+//2
+console.log(a<10 && a>b);
+
+//3
+console.log((a%10==0) && (b%10==0));
+console.log((a%10!==0) && (b%10!==0));
+console.log((a%10===0) && (b%10!==0));
+
+//4
+let c=2345
+console.log(Math.floor(c/1000));
+
+//5
+console.log(c%10);
+
+//6
+let d=2
+let e=9
+console.log(e%d);
+
+//7
+console.log(d*e);
+
+//8
+let m1=50
+let m2=20
+let m3=100
+let total=m1+m2+m3
+console.log(total);
+console.log(total/3);
+
+
+
+
+
+
+
+
